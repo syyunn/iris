590cd60b04a76252e671504cad41ee59bedbf948
jlodoen <jlodoen@mit.edu>
2021-11-12 15:08:52 -0500

    edited file

--- iris.R
+++ iris.R
@@ -7,7 +7,7 @@ iris <- read.csv('/Users/julialodoen/iris/iris.csv')
 # save lm() result as .png
 png('lm_result.png')
 sepal.petal <- lm(iris$petal.length ~ iris$sepal.length)
-plot(iris$sepal.length, iris$petal.length.)
+plot(iris$petal.length, iris$sepal.length)
 abline(sepal.petal)
 dev.off()
 
