bd7b685a09d6d7d2bc9f6e8e27cd501fc6778c68
kdura <durakathryn@gmail.com>
2021-11-12 15:06:25 -0500

    Kathryn edit

--- iris.R
+++ iris.R
@@ -1,13 +1,13 @@
 rm(list = ls())
 
 # change path fit to your local
-setwd('/Users/syyun/iris/')
-iris <- read.csv('/Users/syyun/iris/iris.csv')
+setwd('/Users/kdura/')
+iris <- read.csv('/Users/kdura/iris/iris.csv')
 
 # save lm() result as .png
 png('lm_result.png')
 sepal.petal <- lm(iris$sepal.length ~ iris$petal.length)
-plot(iris$sepal.length, iris$petal.length.)
+plot(iris$petal.length, iris$sepal.length.)
 abline(sepal.petal)
 dev.off()
 
